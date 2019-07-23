53bd0c136d5731bc72d4e70710b8148dc2604823
Cody <cody@Codys-iMac.local>
2019-07-22 17:44:50 -0700

    UI changes and added UI logic

--- Countdown App/Countdown App/ViewController.swift
+++ Countdown App/Countdown App/ViewController.swift
@@ -10,6 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var seconds = 30
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var labelProcedure: UILabel!
+    
+    
+    @IBOutlet weak var sliderOutlet: UISlider!
+    @IBAction func slider(_ sender: UISlider) {
+        
+        positiveSeconds = Int(sender.value)
+        labelTime.text = String(positiveSeconds)
+    }
+    
+    @IBOutlet weak var startOutlet: UIButton!
+    @IBOutlet weak var resetOutlet: UIButton!
+    
+    @IBAction func startButton(_ sender: Any) {
+    
+         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.counter), userInfo: nil, repeats: true)
+        sliderOutlet.isHidden = true
+        startOutlet.isHidden = true
+        
+    }
+    
+    @objc func counter(){
+        if (seconds == 20){
+            labelProcedure.text = "Countdown Started"
+            
+        }
+        
+        seconds -= 1
+        labelTime.text = String(seconds)
+        
+        
+        if (seconds == 15){
+            labelProcedure.text = "Turn Launch and \nIgnitor Safety off"
+            
+        }
+        
+        if (seconds == 2){
+            labelProcedure.text = "Ignition!"
+            
+        }
+        
+        
+        if (seconds == 0)
+        {
+            labelTime.text = String(0)
+            //timer.invalidate()
+            
+            
+            sliderOutlet.isHidden = false
+            startOutlet.isHidden = false
+            
+        }
+    
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+    
+        timer.invalidate()
+        seconds = 30
+        sliderOutlet.setValue(30, animated: true)
+        labelTime.text = "30"
+        labelProcedure.text = "SDSU RP"
+        
+        sliderOutlet.isHidden = false
+        startOutlet.isHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
